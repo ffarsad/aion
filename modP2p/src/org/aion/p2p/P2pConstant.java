ecd39be95b91174d4958971efa7b16ef0c2987b8
Alexandra Roatis <alexandra@nuco.io>
2019-03-25 12:05:23 -0400

    disabling the min nb of peers

--- modP2p/src/org/aion/p2p/P2pConstant.java
+++ modP2p/src/org/aion/p2p/P2pConstant.java
@@ -33,7 +33,7 @@ public class P2pConstant {
              */
             CLOSE_OVERLAPPING_BLOCKS = 15,
             STEP_COUNT = 6,
-            MIN_NORMAL_PEERS = 4,
+            MIN_NORMAL_PEERS = 0,
             MAX_NORMAL_PEERS = 16,
             COEFFICIENT_NORMAL_PEERS = 2,
 
